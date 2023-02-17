9db3748f3f4d0ba5f0818718c83313abe1689b94
arturmx70 <arturmx70@gmail.com>
2023-02-17 14:33:26 +0100

    header scroll code corrected and simplified

--- components/js/main.js
+++ components/js/main.js
@@ -23,57 +23,18 @@ const line2 = document.querySelector(".line2");
   // change header background and links color on scroll
 
   const activeHeader = function() {
-    const button = document.querySelector(".header__button");
     const header = document.querySelector(".header");
 
     if (window.scrollY > 0 || window.pageYOffset > 0) {
-      header.classList.add("header__color");
+      header.classList.add("header__active");
       header.classList.remove("header__transparent");
-      button.classList.remove("white");
-      button.classList.remove("whiteborder");
-      button.classList.add("grey");
-      button.classList.add("greyborder");
-      document.querySelector(".header__nav_links.white").classList.add("grey");
-      document.querySelector(".header__nav_links.white").classList.remove("white");
     } else {
-      header.classList.remove("header__color");
+      header.classList.remove("header__active");
       header.classList.add("header__transparent");
-      button.classList.remove("grey");
-      button.classList.remove("greyborder");
-      button.classList.add("white");
-      button.classList.add("whiteborder");
-      document.querySelector(".header__nav_links").classList.add("white");
-      document.querySelector(".header__nav_links").classList.remove("grey");
     }
   };
   activeHeader();
     window.addEventListener("scroll", activeHeader)
 
-  // const activeHeader = function() {
-  //   const button = document.querySelector(".header__button");
-  //   const header = document.querySelector(".header");
-
-  //   if (window.scrollY > 0 || window.pageYOffset > 0) {
-  //     header.classList.add("header__color");
-  //     header.classList.remove("header__transparent");
-  //     button.classList.remove("white");
-  //     button.classList.remove("whiteborder");
-  //     button.classList.add("grey");
-  //     button.classList.add("greyborder");
-  //     document.querySelector(".header__nav_links.white").classList.add("grey");
-  //     document.querySelector(".header__nav_links.white").classList.remove("white");
-  //   } else {
-  //     header.classList.remove("header__color");
-  //     header.classList.add("header__transparent");
-  //     button.classList.remove("grey");
-  //     button.classList.remove("greyborder");
-  //     button.classList.add("white");
-  //     button.classList.add("whiteborder");
-  //     document.querySelector(".header__nav_links").classList.add("white");
-  //     document.querySelector(".header__nav_links").classList.remove("grey");
-  //   }
-  // };
-  // activeHeader();
-  //   window.addEventListener("scroll", activeHeader)
 
 
